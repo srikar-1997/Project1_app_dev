24d88292a937290a8b1cc377b6fb3cc1a8b1c6bf
srikar-1997 <thalladasrikar7@msitprogram.net>
2020-04-17 17:42:25 +0530

    completed the changes asked to by the reviewer

--- project1/application.py
+++ project1/application.py
@@ -13,24 +13,10 @@ from sqlalchemy import exc
 
 
 app = Flask(__name__)
-os.environ["DATABASE_URL"] = "postgres://vdsjxpbuoklrgr:1669a5d62c3c6e5aaf3c580c4245a3703c760912beb63a870d62f93c69e6b45f@ec2-54-88-130-244.compute-1.amazonaws.com:5432/d8ijt27ijeed12"
+# os.environ["DATABASE_URL"] = "postgres://vdsjxpbuoklrgr:1669a5d62c3c6e5aaf3c580c4245a3703c760912beb63a870d62f93c69e6b45f@ec2-54-88-130-244.compute-1.amazonaws.com:5432/d8ijt27ijeed12"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
-
-# # Check for environment variable
-# if not os.getenv("DATABASE_URL"):
-#     raise RuntimeError("DATABASE_URL is not set")
-
-# # Configure session to use filesystem
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
-
-# # Set up database
-# engine = create_engine(os.getenv("DATABASE_URL"))
-# db = scoped_session(sessionmaker(bind=engine))
-
 
 @app.route("/")
 def index():
@@ -46,15 +32,9 @@ def register():
         pwd = pwd.encode('utf-8')
         hashed = bcrypt.hashpw(pwd, bcrypt.gensalt())
         time_stamp = datetime.datetime.now()
-
-        # reg = Users(username = name, password = pwd, timestamp = time_stamp)
-        # # reg1 = Users.query.get(reg.username)
-        # db.session.add(reg)
-        # db.session.commit()
-        # return render_template("success.html")
         reg = Users(username = name, password = hashed, timestamp = time_stamp)
         try:
-            # reg1 = Users.query.get(reg.username)
+            
             db.session.add(reg)
             db.session.commit()
             return render_template("success.html")
@@ -62,7 +42,7 @@ def register():
             return render_template("userexist.html")
         except:
             print('exception message', 'something went wrong in adding user')
-    # return render_template("userexist.html")
+
         
 @app.route("/admin")
 def admin():
