5149cc48dbd266193b711edcfbb524daaafd17bc
pvsteja <tejapathri6@gmail.com>
2020-04-24 08:53:44 +0530

    displayed books

--- project1/application.py
+++ project1/application.py
@@ -21,64 +21,64 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
 @app.route("/")
 def index():
-    if 'name' not in session :
-        return redirect(url_for('register'))
-    elif session['name'] :
-        return render_template("home.html")
-    
+	if 'name' not in session :
+		return redirect(url_for('register'))
+	elif session['name'] :
+		xyz = Book.query.all()
+		return render_template("home.html", xyz=xyz)
+	
 
 @app.route("/logout")
 def logout():
-    session.clear()
-    return redirect(url_for('register'))
+	session.clear()
+	return redirect(url_for('register'))
 
 @app.route("/register", methods = ["GET", "POST"] )
 def register():
-    if request.method == "GET":
-        return render_template("Registration.html")
-    elif request.form['action'] == 'register':
-        flag = 0
-        name = request.form.get("username")
-        pwd = request.form.get("password")
-        time_stamp = datetime.datetime.now()
-        reg = Users(username = name, password = pwd, timestamp = time_stamp)
-        try:
-            
-            db.session.add(reg)
-            db.session.commit()
-            return render_template("Registration.html", flag_3 = 1)
-        except exc.IntegrityError:
-            flag = 1
-            return render_template("Registration.html", flag = flag)
-            # return render_template("userexist.html")
-        except:
-            print('exception message', 'something went wrong in adding user')
-    elif request.form['action'] == 'login':
-        authentication()
+	if request.method == "GET":
+		return render_template("Registration.html")
+	elif request.form['action'] == 'register':
+		flag = 0
+		name = request.form.get("username")
+		pwd = request.form.get("password")
+		time_stamp = datetime.datetime.now()
+		reg = Users(username = name, password = pwd, timestamp = time_stamp)
+		try:
+			
+			db.session.add(reg)
+			db.session.commit()
+			return render_template("Registration.html", flag_3 = 1)
+		except exc.IntegrityError:
+			flag = 1
+			return render_template("Registration.html", flag = flag)
+			# return render_template("userexist.html")
+		except:
+			print('exception message', 'something went wrong in adding user')
+	elif request.form['action'] == 'login':
+		authentication()
 
 @app.route("/auth", methods = ["GET", "POST"])
 def authentication():
-    if request.method == "POST":
-        name = request.form.get("username")
-        pwd = request.form.get("password")
-        userobj = Users.query.get(name)
-        if userobj:
-            if pwd == userobj.password :
-                session["name"] = name
-                return redirect(url_for('index'))
-                # return redirect(url_for('index'))
-                # if session.get('name') is None:
-            else :
-                return render_template("Registration.html", flag_1= 1)
-        else :
-            return render_template("Registration.html", flag_2 = 1)
+	if request.method == "POST":
+		name = request.form.get("username")
+		pwd = request.form.get("password")
+		userobj = Users.query.get(name)
+		if userobj:
+			if pwd == userobj.password :
+				session["name"] = name
+				return redirect(url_for('index'))
+				# return redirect(url_for('index'))
+				# if session.get('name') is None:
+			else :
+				return render_template("Registration.html", flag_1= 1)
+		else :
+			return render_template("Registration.html", flag_2 = 1)
 
-    
+	
 @app.route("/admin")
 def admin():
-    users = Users.query.order_by(Users.timestamp).all()
-    return render_template("users.html", users = users)
+	users = Users.query.order_by(Users.timestamp).all()
+	return render_template("users.html", users = users)
 
